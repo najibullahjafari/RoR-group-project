0ef7e8f35fdd41b23726459fe3be996689659c9d
rails <najib2020202020@gmail.com>
2023-10-24 17:54:28 +0430

    Fix linter errors

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
-    def new
-      if user_signed_in?
-        redirect_to root_path, notice: "You are already signed in."
-      else
-        super
-      end
+  def new
+    if user_signed_in?
+      redirect_to root_path, notice: 'You are already signed in.'
+    else
+      super
     end
-  
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
