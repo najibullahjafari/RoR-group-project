0521ce6d0cabffc2530204fe4ecb130e4cc8cbbf
rails <najib2020202020@gmail.com>
2023-10-24 17:44:57 +0430

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :authenticate_user!, except: [:new]  # Allow access to login page
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:new]  # Allow access to login page
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
