e51cda53b6ab92b5a01ccb08babee507af1a6d8f
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-26 00:07:11 +0500

    All Logic Done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,14 @@
 class RecipesController < ApplicationController
-    before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :authenticate_user!, only: [:create, :destroy]
   
     def index
-      @recipes = Recipe.where(public: true) 
+      @recipes = Recipe.all
     end
   
     def show
       @recipe = Recipe.find(params[:id])
     end
-  
+    
     def create
       @recipe = current_user.recipes.build(recipe_params)
       if @recipe.save
@@ -20,13 +20,13 @@ class RecipesController < ApplicationController
   
     def destroy
       @recipe = Recipe.find(params[:id])
-      # Your destroy logic should go here to delete the recipe
       if @recipe.destroy
         redirect_to recipes_path, flash: { success: 'Recipe deleted successfully!' }
       else
         redirect_to recipes_path, flash: { error: 'Failed to delete the recipe!' }
       end
     end
+
   
     private
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root to: "home#index", as: :authenticated_root
+      root to: "recipes#index", as: :authenticated_root
     end
 
     unauthenticated do
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   end
 
   resources :foods
-  resources :recipes
+  resources :recipes, only: [:index, :create, :destroy]
+  
 
-  get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
+  get 'public_recipes', to: 'public_recipes#index'
 end
