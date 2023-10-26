fb3ca2af8839b407c3deec228742c0224ffb47dd
rails <najib2020202020@gmail.com>
2023-10-27 03:18:27 +0430

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -23,13 +23,8 @@ Rails.application.routes.draw do
 end
   get '/shopping_list', to: 'shopping_list#show'
 
-<<<<<<< HEAD
-  # get 'shopping_list', to: 'foods#shopping_list', as: 'shopping_list'
-  get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
-=======
   get 'shopping_list', to: 'foods#shopping_list', as: 'shopping_list'
   get 'public_recipes', to: 'public_recipes#index', as: 'public_recipes'
->>>>>>> 8f10ebfa43dc6f73b43246fb0a2d9bbcb0021b62
 
   get 'new_food', to: 'foods#new', as: 'custom_new_food'
   get 'new_recipe', to: 'recipes#new', as: 'custom_new_recipe'
