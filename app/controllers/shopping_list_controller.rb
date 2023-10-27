07e7498868a35bcb9a2af76a1c6350840ce2dd13
najibullah <najib2020202020@gmail.com>
2023-10-27 04:32:23 +0430

    Update shoping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,10 +1,13 @@
 class ShoppingListController < ApplicationController
   before_action :find_user
-  def index
+   def index
+    @user = current_user
     @total_foods = 0
     @total_price = 0
     @recipe_foods = []
+
     @user.recipes.each do |recipe|
+      # Assuming you have a many-to-many relationship between Recipe and RecipeFood
       recipe.recipe_foods.each do |recipe_food|
         @total_foods += 1
         @total_price += recipe_food.food.price * recipe_food.quantity
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, class_name: 'Ingredient'
   belongs_to :user
+  has_many :recipe_foods
 
   def toggle_privacy
     self.public = !public
