ceff412ddffec1a1d7a452e32af2ddf3befa540b
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-28 03:28:12 +0500

    Add Basic Config Files

--- spec/requests/shopping_list_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'ShoppingLists', type: :request do
-  describe 'GET /show' do
-    it 'returns http success' do
-      expect(1).to_be 1
-    end
-  end
-end
