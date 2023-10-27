1ed2959787bec15cdd1582f2e06ca8b098f1bbfa
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-28 01:29:16 +0500

    Public Recipes Page Done

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,8 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.where(public: true)
-
+    @total_foods = Food.count
+    @total_price = Food.sum(:price)
     respond_to do |format|
       format.html
       format.json { render json: @recipes }
