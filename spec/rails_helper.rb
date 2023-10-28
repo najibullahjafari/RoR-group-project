d1b79000a3f1cc9f76cb40d9eda481607fac34bd
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-28 17:37:42 +0500

    Make Img Responsive

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |shoulda_config|
