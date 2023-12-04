f54c4948557e1927ca03ec7618aa249b0b14b29f
MasumaJaffery <masumajaffery1@gmail.com>
2023-12-04 16:45:48 +0500

    Resolve migrations

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 6220231023205552) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
